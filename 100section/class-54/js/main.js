fbce03d01a04f7543cdbe20a155906bf998fa730
habib-ahmed <habibahmedtg@gmail.com>
2017-11-05 15:02:30 +0600

    54

---
+++ 100section/class-54/js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+	"use strict";
+	$(document).ready(function() {   
+
+		// Dropdown-menu set Icon
+		$(".mainmenu ul ul").parent("li").children("a").append(' <i class="fa fa-angle-down"></i>')
+
+		
+	});
+}(jQuery));	
