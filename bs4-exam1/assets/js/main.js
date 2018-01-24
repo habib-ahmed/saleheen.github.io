09ecbdccc143bf69817281d0dca0edf032bcf696
habib-ahmed <habibahmedtg@gmail.com>
2018-01-24 16:33:17 +0600

    bs4-exam1

---
+++ bs4-exam1/assets/js/main.js
@@ -0,0 +1,87 @@
+(function ($) {
+	"use strict";
+
+	$(document).ready(function() {   
+
+		$(".vdo-play-btn").magnificPopup({
+			type: 'video'
+		});
+
+		$(".count").counterUp({
+            delay: 30,
+            time: 2000
+        });
+
+/*	 
+		// mobile-menu active
+		jQuery('.main-menu nav').meanmenu({
+			meanMenuContainer: '.mobile-menu'
+		});
+	 	
+	 	//welcome-area slider active
+        $('.welcome-area').owlCarousel({
+			items: 1,
+			loop: true,
+			nav: false,
+			dot: true,
+			autoplay: true,
+			autoplayTimeout: 5000,
+			autoplayHoverPause: false
+		});
+        
+        // welcome-area slider animation support
+		$(".welcome-area").on("translate.owl.carousel", function(){
+			$(".slider-content").removeClass("animated zoomIn").css({'opacity':'0'});
+		});
+		$(".welcome-area").on("translated.owl.carousel", function(){
+			$(".slider-content").addClass("animated zoomIn").css({'opacity':'1'});
+		});
+		
+
+		$(".team-carousel").owlCarousel({
+			margin: 30,
+			loop: true,
+			nav: true,
+			navText: ["<i class='fa fa-angle-left'</i>","<i class='fa fa-angle-right'</i>"],
+			dots: true,
+			autoplay: true,
+			autoplayTimeout: 2000,
+			autoplayHoverPause: true,
+			responsive: {
+				0:{
+					items:1,
+					nav: false
+				},
+				480:{
+					items:1
+				},
+				768:{
+					items:2
+				},
+				1024:{
+					items:3
+				}
+			}
+		});
+
+
+		// WOW js active
+		new WOW().init()
+
+		//scoll-to-top smoothly
+		jQuery('.back-to-top a ').click(function(){
+			jQuery("html,body").animate({
+				'scrollTop' : '0'
+			}, 1000);
+			return false;
+		});
+        
+ */       
+    });
+	
+	$(window).load(function() {   
+	 
+	
+    });
+	
+}(jQuery));	
